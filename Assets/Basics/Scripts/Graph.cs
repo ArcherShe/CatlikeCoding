d6fe5abe569865ffdb2e43a0022416de2135a351
Archer <sqL.onepiece@outlook.com>
2024-03-14 22:37:00 +0800

    up

--- Assets/Basics/Scripts/Graph.cs
+++ Assets/Basics/Scripts/Graph.cs
@@ -1,14 +1,29 @@
 using UnityEngine;
 
+
+public enum TransitionMode
+{
+    Cycle,
+    Random
+}
+
 public class Graph : MonoBehaviour
 {
     [SerializeField] private Transform pointPrefab;
 
-    [SerializeField, Range(0, 100)] private int resolution = 50;
+    [SerializeField, Range(0, 200)] private int resolution = 50;
 
     [SerializeField] private FunctionLibrary.FunctionName function;
+
+    [SerializeField, Min(0f)] private float functionDuration = 1f, transitionDuration = 1f;
+
+    [SerializeField] private TransitionMode transitionMode;
     
     private Transform[] points;
+    float duration;
+    bool transitioning;
+    FunctionLibrary.FunctionName transitionFunction;
+    
     void Awake () {
         float step = 2f / resolution;
         var scale = Vector3.one * step;
@@ -28,7 +43,38 @@ public class Graph : MonoBehaviour
         }
     }
     
-    void Update () {
+    void Update ()
+    {
+        duration += Time.deltaTime;
+        if (transitioning)
+        {
+            if (duration >= transitionDuration) {
+                duration -= transitionDuration;
+                transitioning = false;
+            }
+        }
+        else if (duration >= functionDuration) {
+            duration -= functionDuration;
+            transitioning = true;
+            transitionFunction = function;
+            PickNextFunction();;
+        }
+        if (transitioning) {
+            UpdateFunctionTransition();
+        }
+        else {
+            UpdateFunction();
+        }
+    }
+
+    void PickNextFunction () {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    
+    void UpdateFunction ()
+    {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
         float step = 2f / resolution;
@@ -42,6 +88,30 @@ public class Graph : MonoBehaviour
             float u = (x + 0.5f) * step - 1f;
             //float v = (z + 0.5f) * step - 1f;
             points[i].localPosition = f(u, v, time);
+        }
+    }
+    
+    void UpdateFunctionTransition ()
+    {
+        FunctionLibrary.Function 
+            from = FunctionLibrary.GetFunction(transitionFunction),
+            to = FunctionLibrary.GetFunction(function);
+        float progress = duration / transitionDuration;
+        
+        float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+                x = 0;
+                z += 1;
+                v = (z + 0.5f) * step - 1f;
+            }
+            float u = (x + 0.5f) * step - 1f;
+            //float v = (z + 0.5f) * step - 1f;
+            points[i].localPosition = FunctionLibrary.Morph(
+                u, v, time, from, to, progress
+            );
         }
     }
 }
